43983a94089f43fe1299136f20d01a0c284de205
haole2k4 <ahihihaodepzai@gmail.com>
2023-11-10 13:42:56 +0700

    Upload
    tải lên cái file cần thiết cho giao diện của  web

---
+++ scipt.js
@@ -0,0 +1,10 @@
+
+function searchingDisplay() {
+    let element = document.querySelector(".searching");
+    element.style.display = "flex";
+}
+
+function nonSearchingDisplay() {
+    let element = document.querySelector(".searching");
+    element.style.display = "none";
+}
